287248d75dec9e45b4b52f5dcf52e31a588e567c
Pablo <pablolucio_@hotmail.com>
2024-08-01 08:19:26 -0300

    fix: fixes certificates blob link

--- src/screens/app/ConsultCertificates/components/SeeCertificateModal/index.tsx
+++ src/screens/app/ConsultCertificates/components/SeeCertificateModal/index.tsx
@@ -4,8 +4,8 @@ import { Title } from "@/components/typography/Title";
 import { ICertificate } from "@/interfaces/dtos/Certificate";
 import { useThemeStore } from "@/store/theme";
 import {
-    reactModalCustomStyles,
-    reactModalCustomStylesDark,
+  reactModalCustomStyles,
+  reactModalCustomStylesDark,
 } from "@/styles/react-modal";
 import { KeyboardEvent, MouseEvent } from "react";
 import Modal from "react-modal";
@@ -64,7 +64,7 @@ export function SeeCertificateModal({
       </div>
       <div className="flex flex-row w-full my-4">
         <img
-          src="https://pscodepscoursetrack.blob.core.windows.net/certificates/treinamento-de-react-pablo-santana-silva2-certificado.png"
+          src="https://pscodepscoursetrack.blob.core.windows.net/certificates/curso-de-react-pablo-santana-silva2-certificado.png"
           width={600}
           height={300}
           alt="certifado_de_conclusao"
