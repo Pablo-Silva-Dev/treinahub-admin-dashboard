0528f8236a2068920573e78d621349197f5670e4
Pablo <pablolucio_@hotmail.com>
2024-09-02 11:34:21 -0300

    styles: improves button and loading styles

--- src/components/buttons/Button/index.tsx
+++ src/components/buttons/Button/index.tsx
@@ -9,7 +9,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ title, isLoading, ...rest }: ButtonProps) {
   return (
     <button
-      className={`w-full h-[52px] flex items-center justify-center bg-primary normal-case lg:text-base text-sm font-medium font-poppins rounded-lg disabled:opacity-[0.8] text-gray-50 font-secondary`}
+      className={`w-full h-[52px] flex items-center justify-center bg-primary normal-case lg:text-base text-sm font-medium font-poppins rounded-lg disabled:opacity-[0.5] text-gray-50 font-secondary`}
       {...rest}
     >
       {isLoading ? <Loading hideText /> : title}
--- src/components/miscellaneous/Loading/index.tsx
+++ src/components/miscellaneous/Loading/index.tsx
@@ -11,10 +11,10 @@ export function Loading({ type, color, text, hideText }: LoadingProps) {
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <ReactLoading
-        type={type ? type : "spin"}
+        type={type ? type : "bars"}
         color={color ? color : "#FFFFFF"}
-        width={24}
-        height={24}
+        width={32}
+        height={32}
       />
       {!hideText && (
         <span className="text-gray-800 dark:text-gray-50 text-[11px] lg:text-sm font-secondary mt-2">
