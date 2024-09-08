acd3a5257594e7b82eeefa245c6ed603ad0c2c28
Pablo <pablolucio_@hotmail.com>
2024-09-08 08:19:20 -0300

    styles: improves TrainingInfoCard component styles

--- src/screens/app/ManageTrainings/components/TrainingInfoCard/index.tsx
+++ src/screens/app/ManageTrainings/components/TrainingInfoCard/index.tsx
@@ -1,6 +1,3 @@
-import { collapseLongString } from "@/utils/formats";
-import { useState } from "react";
-import { Collapse } from "react-collapse";
 import { MdDelete, MdEdit } from "react-icons/md";
 
 interface TrainingInfoCardProps {
@@ -11,7 +8,7 @@ interface TrainingInfoCardProps {
   onDelete: () => void;
   onSeeTraining: (trainingId: string) => void;
   showsSeeClassesButton: boolean;
-  selectedTrainingId: string | null
+  selectedTrainingId: string | null;
 }
 
 export function TrainingInfoCard({
@@ -22,48 +19,23 @@ export function TrainingInfoCard({
   onEdit,
   onSeeTraining,
   showsSeeClassesButton,
-  selectedTrainingId
+  selectedTrainingId,
 }: TrainingInfoCardProps) {
-  const [isCollapsed, setIsCollapsed] = useState(true);
-
-  const descriptionCollapsedLength = description.length / 2;
-
   return (
     <div className="w-full sm:w-[320px] lg:w-[400px] flex flex-col shadow-sm bg-white dark:bg-slate-700 rounded-md mr-0 md:mr-8 mb-5">
       <img
         src={cover_url}
         alt="info_card_placeholder"
-        className="w-full aspect-auto"
+        className="w-full max-h-[240px] aspect-auto"
       />
       <div className="w-full p-4 flex flex-col ">
-        <span className="text-gray-800 dark:text-gray-50 text-sm md:text-lg font-bold font-secondary mb-1">
+        <span className="text-gray-800 dark:text-gray-50 text-[13px] md:text-[14px] font-bold font-secondary mb-1">
           {training}
         </span>
-        <span className="text-gray-800 dark:text-gray-50 text-[11px] lg:text-sm font-primary text-pretty">
-          {isCollapsed &&
-            collapseLongString(description, descriptionCollapsedLength)}
-        </span>
-        <Collapse isOpened={!isCollapsed}>
+        <div className="w-full h-[80px] overflow-y-auto overflow-x-hidden">
           <span className="text-gray-800 dark:text-gray-50 text-[11px] lg:text-sm font-primary text-pretty">
             {description}
           </span>
-        </Collapse>
-        <div className="w-full flex-row justify-start">
-          {isCollapsed ? (
-            <button
-              className="font-bold text-gray-800 dark:text-gray-50 mt-2 text-[11px] lg:text-sm"
-              onClick={() => setIsCollapsed(false)}
-            >
-              Mostrar mais
-            </button>
-          ) : (
-            <button
-              className=" font-bold text-gray-800 dark:text-gray-50 mt-2 text-[11px] lg:text-sm"
-              onClick={() => setIsCollapsed(true)}
-            >
-              Mostrar menos
-            </button>
-          )}
         </div>
         <div className="w-full h-[1px] bg-gray-200 dark:bg-slate-600 mt-2 mb-4" />
         <div className="flex flex-row justify-between items-center">
